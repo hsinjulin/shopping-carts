8396d680f044e80cc7a5e5e37ca3c6245ffe7e5a
linhsinju <11010148@gm.chihlee.edu.tw>
2023-07-26 00:24:08 +0800

    updata nums to count in car.php

--- car.php
+++ car.php
@@ -2,8 +2,10 @@
     session_start();
 
     $num=$_GET['num'];
+    $nums=$_GET['num'];
 
     $_SESSION['num']= $num;
+    $_SESSION['nums']= $_SESSION['nums'].$num.",";
     
     if($_SERVER['PHP_SELF'] == "/book.html"){
     header("Location: book.html");
