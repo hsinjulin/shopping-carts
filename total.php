f94b2273e13511877f41a33d68704fbf336ac1cf
linhsinju <11010148@gm.chihlee.edu.tw>
2023-07-25 23:54:03 +0800

    change switch to array in total.php

--- total.php
+++ total.php
@@ -5,27 +5,12 @@
     $num = $_SESSION['num'];
     echo "<tr><td colspan='4' style='text-align:center'>您購買的商品資訊如下</td></tr>";
     echo "<tr style='text-align:center'><td>項次</td><td>產品代號</td><td>產品名稱</td><td>價格</td></tr>";
-    switch ($num) {
-        case 'book1':
-            $name='超圖解 Arduino 互動設計入門 第3版';
-            $cost='680';
-            break;
-
-        case 'book2':
-            $name='第一次學 Python 就上手！';
-            $cost='480';
-            break;
-
-        case 'cd1':
-            $name='LIR: 王友良+松岡李那+R-ICE 松岡李那《全彩寫真》+卡通版野口資料夾 (CD+贈品A)';
-            $cost='359';
-            break;
-
-        default:
-            $name='李千娜 / 愛 到站了';
-            $cost='459';
-            break;
-    }
+    $arr = array(
+        'book1' => array('book1','超圖解 Arduino 互動設計入門 第3版','680'),
+        'book2' => array('book2','第一次學 Python 就上手！','480'),
+        'cd1' => array('cd1','LIR: 王友良+松岡李那+R-ICE 松岡李那《全彩寫真》+卡通版野口資料夾 (CD+贈品A)','359'),
+        'cd2' => array('cd2','李千娜 / 愛 到站了','459')        
+    );
     $total = 0;
     $total += $cost;
     $numberOfStrings = count($num);
